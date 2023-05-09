74ddbe45c2b36a639149c46b87be1af5acc91a95
Archimondee <kikchiemedia@gmail.com>
2023-05-10 01:41:59 +0700

    Initial Commit

---
+++ uber-ride/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uber-ride
+//
+//  Created by Gilang Aditya Rahman on 10/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
